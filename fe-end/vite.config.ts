a22719b5658c2323f6d07dfa9bc3dd4b33960496
Evan Zuo <v_wangxiangbo01@baidu.com>
2022-04-02 16:39:42 +0800

    feat: fix path

--- fe-end/vite.config.ts
+++ fe-end/vite.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-01 14:07:24
- * @LastEditTime: 2022-04-01 14:26:12
+ * @LastEditTime: 2022-04-02 16:39:17
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /vue-nestjs-ci-cd/fe-end/vite.config.ts
@@ -14,10 +14,10 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      '/api': {
+      "/api": {
         target: "http://localhost:5000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
     },
   },
