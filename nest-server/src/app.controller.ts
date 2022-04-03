20a4746caeb1b99096d2c842055b9bf0e4a57bc1
Evan Zuo <v_wangxiangbo01@baidu.com>
2022-04-03 12:01:32 +0800

    test: push events

--- nest-server/src/app.controller.ts
+++ nest-server/src/app.controller.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-01 13:13:11
- * @LastEditTime: 2022-04-03 11:31:12
+ * @LastEditTime: 2022-04-03 12:01:22
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /vue-nestjs-ci-cd/nest-server/src/app.controller.ts
@@ -27,6 +27,7 @@ export class AppController {
   updateWebHooks(): string {
     return this.appService.updateWebHooks();
   }
+
 
 
 }
