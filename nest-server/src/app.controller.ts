e4246bb270be246110b03618fd96407a915e6575
Evan Zuo <v_wangxiangbo01@baidu.com>
2022-04-03 11:02:58 +0800

    feat: http methods

--- nest-server/src/app.controller.ts
+++ nest-server/src/app.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+/*
+ * @Author: your name
+ * @Date: 2022-04-01 13:13:11
+ * @LastEditTime: 2022-04-03 11:00:52
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /vue-nestjs-ci-cd/nest-server/src/app.controller.ts
+ */
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -15,5 +23,8 @@ export class AppController {
     return this.appService.getInitData();
   }
 
-
+  @Post('/update')
+  updateWebHooks(): string {
+    return this.appService.updateWebHooks();
+  }
 }
--- nest-server/src/app.service.ts
+++ nest-server/src/app.service.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-01 13:13:11
- * @LastEditTime: 2022-04-01 14:35:10
+ * @LastEditTime: 2022-04-03 10:59:33
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /vue-nestjs-ci-cd/nest-server/src/app.service.ts
@@ -17,6 +17,10 @@ export class AppService {
   }
 
   getInitData(): string {
-    return `${this.time}`
+    return `${this.time}`;
+  }
+
+  updateWebHooks(): string {
+    return `updateWebHooks`;
   }
 }
