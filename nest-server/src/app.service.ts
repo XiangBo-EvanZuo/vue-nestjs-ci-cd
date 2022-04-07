317f2639fbbf04d300c3ca31c25b0cc8f9a3e08e
Evan Zuo <v_wangxiangbo01@baidu.com>
2022-04-07 18:03:52 +0800

    feat: styles

--- nest-server/src/app.service.ts
+++ nest-server/src/app.service.ts
@@ -13,7 +13,7 @@ export class AppService {
   time = 0;
   getHello(): string {
     this.time++;
-    return 'Hello World!-RD-CI/CD -1 bibibili' + this.time;
+    return 'Hello World!-RD-CI/CD -1 bibibili - test-nev' + this.time;
   }
 
   getInitData(): string {
