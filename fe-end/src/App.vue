274eef703bff5f4435c55173e1498d21ccfdf56e
Evan Zuo <v_wangxiangbo01@baidu.com>
2022-04-08 20:55:55 +0800

    featL page

--- fe-end/src/App.vue
+++ fe-end/src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-04-08 18:21:12
- * @LastEditTime: 2022-04-08 19:41:36
+ * @LastEditTime: 2022-04-08 20:55:49
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /fe-end/src/App.vue
@@ -14,7 +14,7 @@
 
 <template>
 <div>
-  router-view1111
+  router-view1111777
   <router-view/>
 </div>
 </template>
