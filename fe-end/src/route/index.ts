8335f818e0d3a9b9168c24f6af2daf9e0298872a
Evan Zuo <v_wangxiangbo01@baidu.com>
2022-04-09 17:29:47 +0800

    feat: router import type

--- fe-end/src/route/index.ts
+++ fe-end/src/route/index.ts
@@ -1,16 +1,16 @@
 /*
  * @Author: your name
  * @Date: 2022-04-08 17:57:51
- * @LastEditTime: 2022-04-09 11:36:03
+ * @LastEditTime: 2022-04-09 17:29:26
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /vue-nestjs-ci-cd/fe-end/src/route/index.ts
  */
 import { createRouter, createWebHistory, createWebHashHistory } from "vue-router";
 
-const Home = import("@/components/Home.vue");
-const ErrorPage = import("@/components/Error.vue");
-const About = import("@/components/HelloWorld.vue");
+const Home = () => import("@/components/Home.vue");
+const ErrorPage = () => import("@/components/Error.vue");
+const About = () => import("@/components/HelloWorld.vue");
 
 // 2. Define some routes
 // Each route should map to a component.
