ca1062b19f8900bedd42a0d09937ecd8572e8775
Evan Zuo <v_wangxiangbo01@baidu.com>
2022-05-05 21:01:36 +0800

    feat: mirco-service

--- micro-service/math/ms-app/src/main.ts
+++ micro-service/math/ms-app/src/main.ts
@@ -1,8 +1,16 @@
+/*
+ * @Author: Evan Zuo v_wangxiangbo01@baidu.com
+ * @Date: 2022-05-03 13:42:17
+ * @LastEditors: Evan Zuo v_wangxiangbo01@baidu.com
+ * @LastEditTime: 2022-05-03 20:45:49
+ * @FilePath: /vue-nestjs-ci-cd/micro-service/math/ms-app/src/main.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3025);
 }
 bootstrap();
--- micro-service/math/ms-math-interflow/src/main.ts
+++ micro-service/math/ms-math-interflow/src/main.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2022-04-26 18:06:25
- * @LastEditTime: 2022-04-29 19:11:28
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-05-05 20:57:09
+ * @LastEditors: Evan Zuo v_wangxiangbo01@baidu.com
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /ms-math/src/main.ts
  */
@@ -10,17 +10,32 @@ import { NestFactory } from '@nestjs/core';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-      options: {
-        host: 'localhost',
-        port: 8029,
-      },
+  /**
+   * This example contains a hybrid application (HTTP + TCP)
+   * You can switch to a microservice with NestFactory.createMicroservice() as follows:
+   *
+   * const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+   *  transport: Transport.TCP,
+   *  options: { retryAttempts: 5, retryDelay: 3000 },
+   * });
+   * await app.listen();
+   *
+   */
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.REDIS,
+    options: {
+      retryAttempts: 5,
+      retryDelay: 3000,
+      url: 'redis://81.70.221.165:6379',
     },
-  );
-  app.listen();
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(8087);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
+
